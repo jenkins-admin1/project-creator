f0c76e3c6ceb9ce1cc04726c8a884c23217d4e7d
jenkins-admin1 <128088934+jenkins-admin1@users.noreply.github.com>
2023-03-18 14:47:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,27 +4,17 @@ provider "google" {
 }
 
 resource "google_project" "new_project" {
-  name            = "New Project"
-  project_id      = "new-project-id"
+  name           = "New Project"
+  project_id     = "my-new-project-id-123"
   billing_account = "010BAA-5F9EE7-B2E020"
 }
 
-resource "google_project_service" "all_apis" {
+resource "google_project_service" "compute_engine_api" {
   project = google_project.new_project.project_id
+  service = "compute.googleapis.com"
+}
 
-  service {
-    name = "compute.googleapis.com"
-  }
-
-  service {
-    name = "iam.googleapis.com"
-  }
-
-  service {
-    name = "cloudresourcemanager.googleapis.com"
-  }
-
-  service {
-    name = "cloudbilling.googleapis.com"
-  }
+resource "google_project_service" "network_api" {
+  project = google_project.new_project.project_id
+  service = "networkmanagement.googleapis.com"
 }
