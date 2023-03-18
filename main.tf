1a7ce7054be6890b25cefaab67d06bcf9247463a
jenkins-admin1 <128088934+jenkins-admin1@users.noreply.github.com>
2023-03-18 15:12:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,17 @@
 provider "google" {
   project = "terraform-learning-371709"
   region  = "us-central1"
+  zone    = "us-central1-a"
 }
 
-resource "google_project" "new_project" {
-  name           = "New Project"
-  project_id     = "my-new-project-id-123"
-  billing_account = "010BAA-5F9EE7-B2E020"
+resource "google_project_service" "workflows" {
+  service = "workflows.googleapis.com"
+
+  disable_on_destroy = true
 }
 
-resource "google_project_service" "compute_engine_api" {
-  project = google_project.new_project.project_id
-  service = "compute.googleapis.com"
-}
+resource "google_project_service" "vpc_access" {
+  service = "vpcaccess.googleapis.com"
 
-resource "google_project_service" "network_api" {
-  project = google_project.new_project.project_id
-  service = "networkmanagement.googleapis.com"
+  disable_on_destroy = true
 }
