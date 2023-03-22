2faf27ac2fe0f68276601e867dd3fac87d8ba0e7
jenkins-admin1 <128088934+jenkins-admin1@users.noreply.github.com>
2023-03-22 10:43:09 +0000

    started the source module

---
+++ src/main.tf
@@ -0,0 +1,23 @@
+locals {
+  _defaults = yamldecode(file(var.defaults_file))
+  projects = {
+    for f in fileset("${var.data_dir}", "**/*.yaml") :
+    trimsuffix(f, ".yaml") => merge(
+      yamldecode(file("${var.data_dir}/${f}")),
+      local._defaults
+    )
+  }
+}
+
+# Module invocation to public project-factory module.
+module "projects" {
+  source   = "terraform-google-modules/project-factory/google"
+  version  = "~> 14.1"
+  for_each = local.projects
+
+  name              = each.key
+  org_id            = each.value.org_id
+  billing_account   = each.value.billing_account
+  random_project_id = true
+  folder_id         = each.value.folder_id
+}
