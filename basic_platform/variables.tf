b5e738c716d0e2d519157f9797ff6bebc3ec755b
jenkins-admin1 <128088934+jenkins-admin1@users.noreply.github.com>
2023-03-24 01:41:56 +0530

    Update variables.tf

--- basic_platform/variables.tf
+++ basic_platform/variables.tf
@@ -1,9 +1,11 @@
 variable "billing_account" {
   type = string
+  default = "0110C3-10BB91-8EDC2A"
 }
 
 variable "org_id" {
   type = string
+  default = "837910092674"
 }
 
 variable "state_bucket_name" {
